ebcfee5f29ba5fd794861bea9eca5ef3563a11b6
pkAlphaDragon <pratyushkumartheking@gmail.com>
2020-12-27 18:30:01 +0530

    On branch master Changes to be committed:     modified:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//Created by Pratyush Kumar
+//
+//Copyright and License not decided yet
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+//Create a random grid creating function
+
+//Create a reveal function
+//Use two grids, one for showing, one for storing and functions
+
+int main () {
+    cout << "Hello MineSweeper!";
+
+    return 0;
+}
